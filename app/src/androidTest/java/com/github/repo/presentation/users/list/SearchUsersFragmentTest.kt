e0938e25dc1ede61a9cb58d9f746bbccc944fa2a
Nika <nikolozamgalo@gmail.com>
2021-12-12 18:47:44 +0400

    fix androidTest bug

--- app/src/androidTest/java/com/github/repo/presentation/users/list/SearchUsersFragmentTest.kt
+++ app/src/androidTest/java/com/github/repo/presentation/users/list/SearchUsersFragmentTest.kt
@@ -1,8 +1,9 @@
 package com.github.repo.presentation.users.list
 
-import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
@@ -11,10 +12,13 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.repo.HiltTestActivity
 import com.github.repo.R
 import com.github.repo.presentation.utils.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -29,18 +33,27 @@ class SearchUsersFragmentTest {
 
     private lateinit var stringToBeTyped: String
 
+    private lateinit var navController: NavController
+    private lateinit var hiltActivityScenario: ActivityScenario<HiltTestActivity>
+
     @Before
     fun setUp() {
         hiltRule.inject()
         stringToBeTyped = "nmgalo"
+
+        navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        hiltActivityScenario = launchFragmentInHiltContainer<SearchUsersFragment> {
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(requireView(), navController)
+        }
     }
 
     @Test
-    fun should_showOneItem_whenEnterMyGithubUsername() {
-        launchFragmentInHiltContainer<SearchUsersFragment> {
-            onView(withId(R.id.search)).perform(typeText(stringToBeTyped), closeSoftKeyboard())
-            onView(withId(R.id.usersList)).check(matches(hasChildCount(1)))
-        }
+    fun should_showOneItem_whenEnterMyGithubUsername() = runBlocking {
+        onView(withId(R.id.search)).perform(typeText(stringToBeTyped), closeSoftKeyboard())
+        delay(1500)
+        onView(withId(R.id.usersList)).check(matches(hasChildCount(1)))
+        Unit
     }
 
 }
--- app/src/androidTest/java/com/github/repo/presentation/utils/HiltExt.kt
+++ app/src/androidTest/java/com/github/repo/presentation/utils/HiltExt.kt
@@ -9,23 +9,25 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import com.github.repo.HiltTestActivity
 import com.github.repo.R
-import dagger.hilt.internal.Preconditions
 
 inline fun <reified T : Fragment> launchFragmentInHiltContainer(
     fragmentArgs: Bundle? = null,
     @StyleRes themeResId: Int = R.style.FragmentScenarioEmptyFragmentActivityTheme,
     crossinline action: Fragment.() -> Unit = {}
-) {
+): ActivityScenario<HiltTestActivity> {
     val startActivityIntent = Intent.makeMainActivity(
         ComponentName(
             ApplicationProvider.getApplicationContext(),
             HiltTestActivity::class.java
         )
+    ).putExtra(
+        "androidx.fragment.app.testing.FragmentScenario.EmptyFragmentActivity"
+                + ".THEME_EXTRAS_BUNDLE_KEY", themeResId
     )
 
-    ActivityScenario.launch<HiltTestActivity>(startActivityIntent).onActivity { activity ->
+    return ActivityScenario.launch<HiltTestActivity>(startActivityIntent).onActivity { activity ->
         val fragment: Fragment = activity.supportFragmentManager.fragmentFactory.instantiate(
-            Preconditions.checkNotNull(T::class.java.classLoader),
+            androidx.core.util.Preconditions.checkNotNull(T::class.java.classLoader),
             T::class.java.name
         )
         fragment.arguments = fragmentArgs
@@ -34,6 +36,6 @@ inline fun <reified T : Fragment> launchFragmentInHiltContainer(
             .add(android.R.id.content, fragment, "")
             .commitNow()
 
-        fragment.action()
+        (fragment as T).action()
     }
 }
